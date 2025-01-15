fc90130e9dc8d8362d136e3ea352d79c249b4632
NigelFerrefe <nferrefe@gmail.com>
2025-01-15 17:47:45 +0100

    ready for deploy

--- src/app/ui/components/nav-links.tsx
+++ src/app/ui/components/nav-links.tsx
@@ -23,7 +23,7 @@ const links = [
   { name: 'Contact', href: '/blog/contact', icon: EnvelopeIcon },
 ];
 
-export default async function NavLinks() {
+export default function NavLinks() {
   const pathname = usePathname();
   return (
     <>
@@ -33,9 +33,12 @@ export default async function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-purple-100 md:flex-none md:justify-start md:p-2 md:px-3"
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-purple-100 md:flex-none md:justify-start md:p-2 md:px-3',
+              { 'bg-purple-100': pathname === link.href }
+            )}
           >
-            {/* <LinkIcon className="w-6" /> */}
+            <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
           </Link>
         );
