ea5d8b1db8d6540c52abb404470cd659c00247bc
NigelFerrefe <nferrefe@gmail.com>
2025-01-17 17:04:10 +0100

    api route

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,31 @@
+import { sql } from "@vercel/postgres";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const posts = await sql`SELECT * FROM posts;`;
+    return NextResponse.json({ posts }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+  }
+}
+
+export async function POST(request: Request) {
+  const { searchParams } = new URL(request.url);
+  //This is for getting the body of the request
+  const id = searchParams.get("id");
+  const title = searchParams.get("title");
+  const content = searchParams.get("content");
+  const date = searchParams.get("date");
+  //All those values are going to be inserted into the database
+  //We have to use the sql tag to insert the values into the database
+
+  try {
+    // SQL query to insert the values into the database
+    await sql`INSERT INTO posts (id, author, title, content, date) VALUES (${id}, 'Nigel' , ${title}, ${content}, ${date})`;
+    return NextResponse.json({ message: "Post created" }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json({ error }, { status: 500 });
+    
+  }
+}
--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,6 +1,8 @@
 import Post from "@/app/ui/components/posts/Post";
 import { connectToDB, getPosts } from "@/app/lib/databse";
 import { Button } from "@/app/ui/components/button";
+import Link from "next/link";
+
 
 export default async function Page() {
   const client = await connectToDB();
@@ -13,7 +15,7 @@ export default async function Page() {
     <>
       {client && <h1 className="text-green-500">Connected to database</h1>}
       <h1>Posts</h1>
-      <Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">New +</Button>
+      <Link href="/blog/post/insert"><Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">New +</Button></Link>      
       {posts?.map((post) => (
         <Post 
           key={post.id} 
@@ -29,3 +31,6 @@ export default async function Page() {
 
 //In <Post></Post> we have to define the props that we are passing to the component
 //If we don't do that, we will get an error because the component is expecting props
+//In typescript we need to define the type of the props that we are passing to the component
+//Because it need props to be passed to it, in javascript we can pass props without defining the type of the props
+//For example: <Post id={post.id} title={post.title} content={post.content} date={post.date} />
