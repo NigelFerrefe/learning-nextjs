eaf67b0944d31bc2b8ff9fdfb8b4e8a788b86cc0
NigelFerrefe <nferrefe@gmail.com>
2025-01-20 11:21:02 +0100

    add sigin/signup

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,6 @@
 import { sql } from "@vercel/postgres";
 import { NextResponse } from "next/server";
+import { auth } from "../../../../auth.config";
 
 export async function GET() {
   try {
@@ -11,6 +12,10 @@ export async function GET() {
 }
 
 export async function POST(request: Request) {
+  //Get auth
+  const session = await auth()
+
+
   const { searchParams } = new URL(request.url);
   //This is for getting the body of the request
   const id = searchParams.get("id");
@@ -19,10 +24,16 @@ export async function POST(request: Request) {
   const date = searchParams.get("date");
   //All those values are going to be inserted into the database
   //We have to use the sql tag to insert the values into the database
+  const author = searchParams.get('author')
+
+  
 
   try {
+    if(!session) {
+      return NextResponse.json({ message: "User not authenticated" }, { status: 401 });
+    }
     // SQL query to insert the values into the database
-    await sql`INSERT INTO posts (id, author, title, content, date) VALUES (${id}, 'Nigel' , ${title}, ${content}, ${date})`;
+    await sql`INSERT INTO posts (id, author, title, content, date) VALUES (${id}, ${author} , ${title}, ${content}, ${date})`;
     return NextResponse.json({ message: "Post created" }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ error }, { status: 500 });
--- src/app/blog/posts/page.tsx
+++ src/app/blog/posts/page.tsx
@@ -1,28 +1,34 @@
 import Post from "@/app/ui/components/posts/Post";
 import { connectToDB, getPosts } from "@/app/lib/databse";
 import { Button } from "@/app/ui/components/button";
+import { auth } from "../../../../auth.config";
 import Link from "next/link";
-
 
 export default async function Page() {
   const client = await connectToDB();
   //client is a const that holds the connection to the database
   const posts = await getPosts();
   //posts is a const that holds the data from the database
-  console.log('Posts data:', posts);
+  const session = await auth();
 
   return (
     <>
       {client && <h1 className="text-green-500">Connected to database</h1>}
       <h1>Posts</h1>
-      <Link href="/blog/post/insert"><Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">New +</Button></Link>      
+      {session?.user && (
+        <Link href="/blog/post/insert">
+          <Button className="outline outline-1  border-purple-700 text-purple-700 hover:bg-purple-700 hover:text-white my-5 py-2 px-4 rounded">
+            New +
+          </Button>
+        </Link>
+      )}
       {posts?.map((post) => (
-        <Post 
-          key={post.id} 
-          id={post.id} 
-          title={post.title} 
-          content={post.content} 
-          date={post.date} 
+        <Post
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          content={post.content}
+          date={post.date}
         />
       ))}
     </>
